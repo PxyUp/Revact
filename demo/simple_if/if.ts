c30b7fce66f6c3584c1735285705fe08f4ccdab3
Iurii Panarin <tduble94@gmail.com>
2019-03-24 09:05:33 +0100

    proper demo with if in component

--- demo/simple_if/if.ts
+++ demo/simple_if/if.ts
@@ -1,20 +1,28 @@
 import { Component, FastDomNode, createComponent, fdIf, fdReactiveValue } from "../../src";
 
+import { createTimer } from "../timer/timer";
+
 export function createIf() {
     return createComponent(IfWithChild);
 }
 
 class IfWithChild extends Component {
     reactive = {
-        show: fdIf(true)
+        show: fdIf(true),
+        text: fdReactiveValue("Here timer")
     }
 
     get show() {
         return this.reactive.show;
     }
 
+    get text() {
+        return this.reactive.text
+    }
+
     onClick = () => {
         this.show.value = !this.show.value;
+        this.text.value = this.show.value ? "Here timer" : "Sorry not timer"
     }
 
     template: FastDomNode = {
@@ -30,7 +38,7 @@ class IfWithChild extends Component {
                    {
                        tag: "span",
                        textValue: this.show
-                   }
+                   },
                ]
            },
            {
@@ -48,6 +56,18 @@ class IfWithChild extends Component {
                 tag: "div",
                 skip: this.show,
                 textValue: "You will sometimes"
+            }, {
+                tag: "div",
+                children: [
+                    {
+                        tag: "strong",
+                        textValue: this.text
+                    },
+                    {
+                        ...createTimer(),
+                        skip: this.show,
+                    }
+                ]
             }
        ]
     }
